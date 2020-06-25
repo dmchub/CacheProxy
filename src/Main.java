73399e3d2f86023bf7a9034efd11b91a230a692f
dmchub <37676729+dmchub@users.noreply.github.com>
2020-06-25 04:17:59 +0300

    Test for nacked and cached methods

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import com.sun.deploy.net.proxy.ProxyUtils;
+
 public class Main {
     public static void main(String[] args) {
-        run(new CachedCalculator(new CalculatorImpl()));
+        // run(new CachedCalculator(new CalculatorImpl()));
+        Calculator calculator = new CalculatorImpl();
+        calculator.calc(1);
+        calculator.calc(1);
+
+        Calculator cached = new CachedCalculator(calculator);
+        cached.calc(1);
+        cached.calc(2);
+        cached.calc(1);
     }
 
     private static void run(Calculator calculator) {
